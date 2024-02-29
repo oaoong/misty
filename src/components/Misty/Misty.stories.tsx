d6ebc63176aaf3049e29417be103873c9e2d6b94
oaoong <pizzachicken@kakao.com>
2024-02-29 17:21:10 +0900

    :memo: add storybook docs

--- src/components/Misty/Misty.stories.tsx
+++ src/components/Misty/Misty.stories.tsx
@@ -1,145 +1,139 @@
-import type { StoryObj } from '@storybook/react'
-import { Misty, MistyBody, MistyClose, MistyDescription } from '@/components'
+import { Misty, MistyDescription, MistyClose, MistyBody } from '@/components'
+import type { Meta, StoryObj } from '@storybook/react'
 
 const meta = {
   title: 'UI/Misty',
+  component: Misty,
   tags: ['autodocs'],
-  argTypes: {},
-}
+  argTypes: {
+    children: {
+      control: {
+        type: 'object',
+      },
+    },
+    contents: {
+      control: {
+        type: 'object',
+      },
+    },
+    preLoad: {
+      control: {
+        type: 'boolean',
+      },
+    },
+    isOpen: {
+      control: {
+        type: 'boolean',
+      },
+    },
+    style: {
+      control: {
+        type: 'object',
+      },
+    },
+    width: {
+      control: {
+        type: 'text',
+      },
+    },
+    height: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+} satisfies Meta<typeof Misty>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
 export const Normal: Story = {
-  render: () => (
-    <>
-      <div
+  args: {
+    children: (
+      <MistyBody>
+        <MistyClose />
+        <MistyDescription>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sunt placeat nostrum doloribus aut ex veniam
+          perspiciatis expedita, cupiditate dolor rem quod eaque doloremque cum quisquam sit illum repellendus
+          provident. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sunt placeat nostrum doloribus aut
+          ex veniam perspiciatis expedita, cupiditate dolor rem quod eaque doloremque cum quisquam sit illum repellendus
+          provident. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sunt placeat nostrum doloribus aut
+          ex veniam perspiciatis expedita, cupiditate dolor rem quod eaque doloremque cum quisquam sit illum repellendus
+          provident.
+        </MistyDescription>
+      </MistyBody>
+    ),
+    contents: (
+      <div style={{ padding: '2rem' }}>
+        <button type="button">Contents</button>
+      </div>
+    ),
+    preLoad: true,
+    isOpen: true,
+    style: { backgroundColor: 'powderblue' },
+  },
+}
+
+export const Preload: Story = {
+  args: {
+    children: (
+      <MistyBody
         style={{
-          display: 'flex',
-          justifyContent: 'center',
-          width: '100%',
+          background: `linear-gradient(217deg, rgba(255,0,0,.8), rgba(255,0,0,0) 70.71%),
+        linear-gradient(127deg, rgba(0,255,0,.8), rgba(0,255,0,0) 70.71%),
+        linear-gradient(336deg, rgba(0,0,255,.8), rgba(0,0,255,0) 70.71%)`,
         }}
       >
-        <Misty
-          width="100%"
-          height="200px"
-          preLoad={false}
-          contents={
-            <div>
-              <div>'hi'</div>
-              <div>'hi'</div>
-              <div>'hi'</div>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis incidunt perferendis cum eius sint
-              error et saepe ut, ad at reiciendis quibusdam praesentium iusto quae voluptatibus excepturi vero?
-              Possimus, vero. Quisquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, reiciendis
-              maxime. Nisi eum cumque tempore obcaecati. Debitis, dolorum doloremque nihil, incidunt, laudantium aliquam
-              ab corrupti pariatur fuga sequi nulla veniam! Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Cum voluptatum qui iure labore odio laborum vero eum, facilis laboriosam culpa. Voluptates numquam atque
-              magni culpa beatae laborum eum rerum impedit. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Perspiciatis incidunt perferendis cum eius sint error et saepe ut, ad at reiciendis quibusdam praesentium
-              iusto quae voluptatibus excepturi vero? Possimus, vero. Quisquam. Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Omnis, reiciendis maxime. Nisi eum cumque tempore obcaecati. Debitis, dolorum doloremque
-              nihil, incidunt, laudantium aliquam ab corrupti pariatur fuga sequi nulla veniam! Lorem ipsum dolor sit
-              amet consectetur, adipisicing elit. Cum voluptatum qui iure labore odio laborum vero eum, facilis
-              laboriosam culpa. Voluptates numquam atque magni culpa beatae laborum eum rerum impedit. Lorem ipsum dolor
-              sit, amet consectetur adipisicing elit. Perspiciatis incidunt perferendis cum eius sint error et saepe ut,
-              ad at reiciendis quibusdam praesentium iusto quae voluptatibus excepturi vero? Possimus, vero. Quisquam.
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, reiciendis maxime. Nisi eum cumque tempore
-              obcaecati. Debitis, dolorum doloremque nihil, incidunt, laudantium aliquam ab corrupti pariatur fuga sequi
-              nulla veniam! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum voluptatum qui iure labore
-              odio laborum vero eum, facilis laboriosam culpa. Voluptates numquam atque magni culpa beatae laborum eum
-              rerum impedit.
-            </div>
-          }
-        >
-          <MistyBody
-            style={{
-              background: `linear-gradient(217deg, rgba(255,0,0,.8), rgba(255,0,0,0) 70.71%),
-          linear-gradient(127deg, rgba(0,255,0,.8), rgba(0,255,0,0) 70.71%),
-          linear-gradient(336deg, rgba(0,0,255,.8), rgba(0,0,255,0) 70.71%)`,
-            }}
-          >
-            <MistyClose />
-            <MistyDescription>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum adipisci expedita iusto nemo earum
-              accusantium sint laborum ratione dicta rem vitae praesentium sapiente nam saepe ipsum magnam, eos et
-              voluptatem?
-            </MistyDescription>
-          </MistyBody>
-        </Misty>
+        <MistyClose />
+        <MistyDescription>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sunt placeat nostrum doloribus aut ex veniam
+          perspiciatis expedita, cupiditate dolor rem quod eaque doloremque cum quisquam sit illum repellendus
+          provident. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sunt placeat nostrum doloribus aut
+          ex veniam perspiciatis expedita, cupiditate dolor rem quod eaque doloremque cum quisquam sit illum repellendus
+          provident. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sunt placeat nostrum doloribus aut
+          ex veniam perspiciatis expedita, cupiditate dolor rem quod eaque doloremque cum quisquam sit illum repellendus
+          provident.
+        </MistyDescription>
+      </MistyBody>
+    ),
+    contents: (
+      <div style={{ padding: '2rem' }}>
+        <button type="button">Contents</button>
       </div>
-      <Misty
-        preLoad={true}
-        contents={
-          <div>
-            <div>'hi'</div>
-            <div>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum adipisci expedita iusto nemo earum
-              accusantium sint laborum ratione dicta rem vitae praesentium sapiente nam saepe ipsum magnam, eos et
-              voluptatem?
-            </div>
-            <div>'hi'</div>
-            <div>'hi'</div>
-            <div>'hi'</div>
-            <div>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum adipisci expedita iusto nemo earum
-              accusantium sint laborum ratione dicta rem vitae praesentium sapiente nam saepe ipsum magnam, eos et
-              voluptatem?
-            </div>
-            <div>'hi'</div>
-            <div>'hi'</div>
-            <div>'hi'</div>
-            <div>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum adipisci expedita iusto nemo earum
-              accusantium sint laborum ratione dicta rem vitae praesentium sapiente nam saepe ipsum magnam, eos et
-              voluptatem?
-            </div>
-            <div>'hi'</div>
-            <div>'hi'</div>
-            <div>'hi'</div>
-          </div>
-        }
-      >
-        <MistyBody>
-          <MistyClose />
-          <MistyDescription style={{ filter: 'blur(2px)' }}>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum adipisci expedita iusto nemo earum
-            accusantium sint laborum ratione dicta rem vitae praesentium sapiente nam saepe ipsum magnam, eos et
-            voluptatem?
-          </MistyDescription>
-        </MistyBody>
-      </Misty>
-      <Misty
-        preLoad={true}
-        contents={
-          <div>
-            <div>'hi'</div>
-            <div>'hi'</div>
-            <div>'hi'</div> <div>'hi'</div>
-            <div>'hi'</div>
-            <div>'hi'</div> <div>'hi'</div>
-            <div>'hi'</div>
-            <div>'hi'</div> <div>'hi'</div>
-            <div>'hi'</div>
-            <div>'hi'</div>
-          </div>
-        }
-      >
-        <MistyBody>
-          <MistyDescription
-            style={{
-              background: `linear-gradient(217deg, rgba(255,0,0,.8), rgba(255,0,0,0) 70.71%),
-          linear-gradient(127deg, rgba(0,255,0,.8), rgba(0,255,0,0) 70.71%),
-          linear-gradient(336deg, rgba(0,0,255,.8), rgba(0,0,255,0) 70.71%)`,
-            }}
-          >
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum adipisci expedita iusto nemo earum
-            accusantium sint laborum ratione dicta rem vitae praesentium sapiente nam saepe ipsum magnam, eos et
-            voluptatem?
-          </MistyDescription>
-          <MistyClose />
-        </MistyBody>
-      </Misty>
-    </>
-  ),
+    ),
+    preLoad: false,
+    isOpen: true,
+    style: {},
+    width: '500px',
+    height: '500px',
+  },
+}
+
+export const Verified: Story = {
+  args: {
+    children: (
+      <MistyBody>
+        <MistyClose />
+        <MistyDescription>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sunt placeat nostrum doloribus aut ex veniam
+          perspiciatis expedita, cupiditate dolor rem quod eaque doloremque cum quisquam sit illum repellendus
+          provident. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sunt placeat nostrum doloribus aut
+          ex veniam perspiciatis expedita, cupiditate dolor rem quod eaque doloremque cum quisquam sit illum repellendus
+          provident. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sunt placeat nostrum doloribus aut
+          ex veniam perspiciatis expedita, cupiditate dolor rem quod eaque doloremque cum quisquam sit illum repellendus
+          provident.
+        </MistyDescription>
+      </MistyBody>
+    ),
+    contents: (
+      <div style={{ padding: '2rem' }}>
+        <button type="button">Contents</button>
+      </div>
+    ),
+    preLoad: false,
+    isOpen: false,
+    style: { backgroundColor: 'powderblue' },
+    width: '1024px',
+    height: '100%',
+  },
 }
