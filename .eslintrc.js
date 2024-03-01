5b38453158b12dc35c6f40309280a8abb4e5951e
oaoong <pizzachicken@kakao.com>
2024-03-01 15:07:23 +0900

    :tada: init cypress

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:cypress/recommended',
     'prettier',
   ],
   plugins: ['@typescript-eslint', 'import', 'react', 'react-hooks'],
